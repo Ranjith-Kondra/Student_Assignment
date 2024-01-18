8fef4c9cf331249c7debacb12df3fc1b0b7ef999
Ranjith Redddy <ranjithreddykondra@gmail.com>
2024-01-18 10:47:02 +0530

    Spring Boot initialized

--- StudentSpringBoot/demo/src/main/java/com/ts/DemoApplication.java
+++ StudentSpringBoot/demo/src/main/java/com/ts/DemoApplication.java
@@ -2,8 +2,12 @@ package com.ts;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication
+@EnableJpaRepositories(basePackages="com.dao")
+@EntityScan(basePackages="com.model")
+@SpringBootApplication(scanBasePackages="com")
 public class DemoApplication {
 
 	public static void main(String[] args) {
@@ -11,3 +15,4 @@ public class DemoApplication {
 	}
 
 }
+
